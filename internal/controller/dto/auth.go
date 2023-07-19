d280d2696ba5c64f2af897039068829bfd62b397
Teleфonчиk <matvey-sizov@mail.ru>
2023-07-19 21:26:30 +0300

    feat: add files interaction

--- internal/controller/dto/auth.go
+++ internal/controller/dto/auth.go
@@ -5,6 +5,8 @@ type Email struct {
 }
 
 type EmailWithCode struct {
-	Email string `json:"email,omitempty" validate:"required,email"`
-	Code  string `json:"code,omitempty" validate:"required,len=5"`
+	Email     string `json:"email,omitempty" validate:"required,email"`
+	Code      string `json:"code,omitempty" validate:"required,len=5"`
+	FirstName string `json:"firstName,omitempty" validate:"omitempty,gte=2,lte=32"`
+	LastName  string `json:"lastName,omitempty" validate:"omitempty,gte=2,lte=32"`
 }
