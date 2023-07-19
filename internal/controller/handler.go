29d3745c2835b5fe7a1d44a6038949583c4b4c33
Teleфonчиk <matvey-sizov@mail.ru>
2023-07-19 21:37:56 +0300

    feat: create services

--- internal/controller/handler.go
+++ internal/controller/handler.go
@@ -4,12 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/koalitz/backend/ent"
 	"github.com/koalitz/backend/internal/controller/dao"
+	"github.com/koalitz/backend/internal/controller/dto"
 	"github.com/koalitz/backend/pkg/conf"
+	"mime/multipart"
 )
 
 type UserService interface {
 	FindUserByID(id int) (*ent.User, error)
-	FindMe(sess *dao.Session) (*dao.Me, error)
 }
 
 type AuthService interface {
@@ -19,6 +20,13 @@ type AuthService interface {
 	CompareHashAndPassword(old, new []byte) error
 
 	AuthUserByEmail(email string) (*ent.User, error)
+	CreateUserByEmail(auth dto.EmailWithCode) (*ent.User, error)
+}
+
+type PostService interface {
+	FindPostByID(id int) (*ent.Post, error)
+	FindPostByTitle(title string) ([]*ent.Post, error)
+	CreatePost(imageName *string, info dto.PostInfo, id int) (*ent.Post, error)
 }
 
 type MailSender interface {
@@ -29,16 +37,18 @@ type Session interface {
 	SetNewCookie(id int, c *gin.Context) error
 	ValidateSession(sessionId string) (info *dao.Session, ok bool, err error)
 	GenerateSecretCode(length int) string
+	GenerateFileName(c *gin.Context, file *multipart.FileHeader) (string, error)
 }
 
 type Handler struct {
 	user UserService
 	auth AuthService
 	mail MailSender
+	post PostService
 	sess Session
 	cfg  *conf.Config
 }
 
-func NewHandler(user UserService, auth AuthService, mail MailSender, sess Session, cfg *conf.Config) *Handler {
-	return &Handler{user: user, auth: auth, mail: mail, sess: sess, cfg: cfg}
+func NewHandler(user UserService, auth AuthService, mail MailSender, post PostService, sess Session, cfg *conf.Config) *Handler {
+	return &Handler{user: user, auth: auth, mail: mail, post: post, sess: sess, cfg: cfg}
 }
