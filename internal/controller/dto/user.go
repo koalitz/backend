a809ee924c52e2efd66f9f9ecc7fba6933c76937
Teleфonчиk <matvey-sizov@mail.ru>
2023-07-19 21:34:27 +0300

    chore: add DTO and DAO structs

--- internal/controller/dto/user.go
+++ internal/controller/dto/user.go
@@ -1,5 +1,24 @@
 package dto
 
-type NameParam struct {
-	Name string `uri:"name" validate:"required,gte=5,lte=20,name"`
+import (
+	"mime/multipart"
+)
+
+type PostInfo struct {
+	Title   string                `form:"title,omitempty" validate:"required,gte=3,lte=70"`
+	Place   string                `form:"place,omitempty" validate:"required,lte=100"`
+	Summary string                `form:"summary,omitempty" validate:"required,lte=1024"`
+	Image   *multipart.FileHeader `form:"file,omitempty"`
+}
+
+type Limit struct {
+	Limit int `uri:"limit"`
+}
+
+type Title struct {
+	Title string `uri:"title"`
+}
+
+type ID struct {
+	ID int `uri:"id"`
 }
